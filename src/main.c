63dfa0df0d520e788505eb6b65e238e26ac4716f
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2019-11-10 20:53:40 +0000

    Read in input, Evaluate, Print

--- src/main.c
+++ src/main.c
@@ -2,16 +2,32 @@
 #include <stdio.h>
 
 #include "tokenise.h"
+#include "ast.h"
+#include "parse.h"
+#include "eval.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
   char **tokens;
   int n_tokens = 0;
-  tokenise("(message '(1 1 1))", &tokens, &n_tokens);
 
-  for (int i = 0; i < n_tokens; i++) {
-    fprintf(stderr, "TOKEN: %s\n", tokens[i]);
+  if (argc > 1) {
+    tokenise(argv[1], &tokens, &n_tokens);
   }
+  else {
+    tokenise("(+ (+ 1 1 1) 1)", &tokens, &n_tokens);
+  }
+
+  struct ast_node *ast = get_ast(tokens, n_tokens);
+  for (int i = 0; i < n_tokens; i++) {
+    free(tokens[i]);
+  }
+
+  char *result = eval(ast);
+
+  fprintf(stderr, "RESULT: %s\n", result);
+  
+  free_ast(ast);
 
   return 0;
 }
