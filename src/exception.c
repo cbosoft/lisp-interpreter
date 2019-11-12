05f2689548d87aa4035c8ca409aee8a2be463eda
Chris <chrismjboyle@gmail.com>
2019-11-12 22:26:09 +0000

    Started to add basic error checking/exception facilities

---
+++ src/exception.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+#include "colour.h"
+#include "exception.h"
+
+#define ERRLEN 256
+
+void assert_or_error(int condition, const char *source, const char *fmt, ...)
+{
+  if (condition)
+    return;
+
+  char message[ERRLEN] = {0};
+  
+  va_list ap;
+
+  va_start(ap, fmt);
+  vsnprintf(message, ERRLEN-1, fmt, ap);
+  va_end(ap);
+
+  fprintf(stderr, BG_RED"Error"RESET" in "FG_BLUE"%s"RESET": %s", source, message);
+  
+  exit(1);
+}
---
+++ src/exception.h
@@ -0,0 +1,1 @@
+void assert_or_error(int condition, const char *source, const char *fmt, ...);
