285a685ca6679f8c9d6a7c4a5e8c524948ecfc62
Chris <chrismjboyle@gmail.com>
2019-11-15 12:46:51 +0000

    Sorted out eval of list, and added operation rule exception for "quote" function
    "quote" returns its arguments, unchanged.

--- src/eval.c
+++ src/eval.c
@@ -19,23 +19,36 @@ LispObject *eval(LispObject *root, LispEnvironment *env)
   LispEnvironment *subenv = NULL;
   int nargs;
 
-  // TODO change to assert_or_error
-
   switch (root->type) {
+
   case LISPOBJECT_SYMBOL:
     return eval(root->value_symbol, env);
     break;
+
   case LISPOBJECT_LIST:
-    fn = root->list_next;
+    fn = root->list_child;
     
     if (fn == NULL)
       return NULL;
+
+    if (fn->type == LISPOBJECT_LIST) {
+      obj_i = fn;
+      if (obj_i->list_next) {
+        fprintf(stderr, "  eval things in list\n");
+        while (obj_i->list_next != NULL) {
+          fprintf(stderr, "  type: %s\n", LispObject_type(obj_i));
+          eval(obj_i, env);
+          obj_i = obj_i->list_next;
+        }
+      }
+      return eval(obj_i, env);
+    }
 	
     args = fn->list_next;
     nargs = LispObject_list_size(args);
 
-    fname = fn->symbol_name != NULL ? fn->symbol_name : fn->value_string;
-    assert_or_error(fname != NULL, "eval", "Cannot evalutate function with null name");
+    fname = fn->symbol_name == NULL ? fn->value_string : fn->symbol_name;
+    assert_or_error(fname != NULL, "eval", "Cannot evaluate function: %s", LispObject_repr(fn));
     struct function_table_entry *tentry = get_function(fname);
     
     if (tentry->lisp_function != NULL) {
@@ -43,9 +56,10 @@ LispObject *eval(LispObject *root, LispEnvironment *env)
       assert_or_error(nargs == tentry->lisp_function->number_required_args, "eval", "number of arguments does not match required (got %d, need %d).", nargs, tentry->lisp_function->number_required_args);
       LispEnvironment *sub_env = environment_new_environment(env);
       obj_i = args;
+
       for (int i = 0; i < nargs; i++) {
-	environment_add_symbol(sub_env, obj_i);
-	obj_i = obj_i->list_next;
+	      environment_add_symbol(sub_env, obj_i);
+	      obj_i = obj_i->list_next;
       }
 
       return eval(tentry->lisp_function->body, subenv);
@@ -53,7 +67,29 @@ LispObject *eval(LispObject *root, LispEnvironment *env)
     }
     else if (tentry->builtin_function != NULL) {
 
-      return tentry->builtin_function(args);
+      LispObject *evaluated_args = NULL, *ev_args_i = NULL;;
+
+      if (strcmp(tentry->name, "quote") == 0) {
+        return args;
+      }
+
+      if (args != NULL) {
+        
+        // evaluate first arg
+        obj_i = args;
+        evaluated_args = eval(args, env);
+        ev_args_i = evaluated_args;
+        obj_i = obj_i->list_next;
+
+        // evaluate subsequent args
+        while (obj_i != NULL) {
+          ev_args_i->list_next = eval(obj_i, env);
+          ev_args_i = ev_args_i->list_next;
+          obj_i = obj_i->list_next;
+        }
+      }
+      
+      return tentry->builtin_function(evaluated_args);
       
     }
     else {
@@ -61,22 +97,8 @@ LispObject *eval(LispObject *root, LispEnvironment *env)
     }
     
     break;
-  default:
-    return root;
   }
   
-  fname = root->value_string;
-  struct function_table_entry *value = get_function(fname);
-
-  // if arg is a list:
-  //  eval all elements in the list
-  // return last element in the list
-
-  if (root->list_next)
-    return eval(root->list_next);
-  else
-    return value;
-
-  // evaluation will de-ref objects; they should be free'd
-  // TODO figure out how to do this
+  // default: root is atom (float, int, string, bool...)
+  return root;
 }
