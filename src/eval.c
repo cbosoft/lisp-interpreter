913434a33658b8681c8de22f322d967a45226204
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2019-11-17 16:33:19 +0000

    Updated eval to check env for symbol value

--- src/eval.c
+++ src/eval.c
@@ -18,9 +18,11 @@
 LispObject *eval(LispObject *root, LispEnvironment *env)
 {
   char *fname = NULL;
-  LispObject *args = NULL, *fn = NULL, *obj_i = NULL;
+  LispObject *args = NULL, *fn = NULL, *obj_i = NULL, *var_obj = NULL;
   LispEnvironment *subenv = NULL;
   int nargs;
+  LispFunction *var_lfunc = NULL;
+  LispBuiltin *var_bfunc = NULL;
 
   debug_message("IN EVAL\n");
 
@@ -37,8 +39,10 @@ LispObject *eval(LispObject *root, LispEnvironment *env)
   switch (root->type) {
 
   case LISPOBJECT_SYMBOL:
-    return eval(root->value_symbol, env);
-    break;
+    LispEnvironment_get(env, root->symbol_name, NULL, NULL, &var_obj);
+    assert_or_error(var_obj != NULL, "eval", "Symbol %s has not value as variable", root->symbol_name);
+    ERROR_CHECK;
+    return var_obj;
 
   case LISPOBJECT_LIST:
     fn = root->list_child;
@@ -64,34 +68,39 @@ LispObject *eval(LispObject *root, LispEnvironment *env)
     args = fn->list_next;
     nargs = LispObject_list_size(args);
 
-    fname = fn->symbol_name == NULL ? fn->value_string : fn->symbol_name;
+    fname = fn->symbol_name;
     assert_or_error(fname != NULL, "eval", "Cannot evaluate function: %s", LispObject_repr(fn));
-    struct function_table_entry *tentry = get_function(fname);
+    ERROR_CHECK;
+
+    assert_or_error(!LispEnvironment_get(env, fname, &var_lfunc, &var_bfunc, &var_obj), "eval", "Object with name \"%s\" not found in environment.", fname);
+    ERROR_CHECK;
+
+
     
-    if (tentry->lisp_function != NULL) {
+    if (var_lfunc != NULL) {
       debug_message("SYMBOL %s IS LISP FUNCTION", fn->symbol_name);
       
-      assert_or_error(nargs == tentry->lisp_function->number_required_args, "eval", "number of arguments does not match required (got %d, need %d).", nargs, tentry->lisp_function->number_required_args);
+      assert_or_error(nargs == var_lfunc->number_required_args, "eval", "number of arguments does not match required (got %d, need %d).", nargs, var_lfunc->number_required_args);
       ERROR_CHECK;
       LispEnvironment *sub_env = LispEnvironment_new_environment(env);
       obj_i = args;
 
       for (int i = 0; i < nargs; i++) {
-	      LispEnvironment_add_symbol(sub_env, obj_i);
+	      LispEnvironment_add_variable(sub_env, var_lfunc->arg_names[i], NULL, NULL, obj_i);
 	      obj_i = obj_i->list_next;
       }
 
-      LispObject *rv = eval(tentry->lisp_function->body, subenv);
+      LispObject *rv = eval(var_lfunc->body, subenv);
       // TODO free subenv
       return rv;
       
     }
-    else if (tentry->builtin_function != NULL) {
+    else if (var_bfunc != NULL) {
       debug_message("SYMBOL %s IS BUILTIN\n", fn->symbol_name);
 
       LispObject *evaluated_args = NULL, *ev_args_i = NULL;
 
-      if (strcmp(tentry->name, "quote") == 0) {
+      if (strcmp(fname, "quote") == 0) {
         return args;
       }
 
@@ -117,7 +126,7 @@ LispObject *eval(LispObject *root, LispEnvironment *env)
         }
       }
       
-      return tentry->builtin_function(evaluated_args, env);
+      return (*var_bfunc)(evaluated_args, env);
       
     }
     else {
