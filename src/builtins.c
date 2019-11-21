35dd2c1e6d6c65c1df4c671bee03a31c414fe2af
Chris <chrismjboyle@gmail.com>
2019-11-21 15:55:28 +0000

    Delete object if exists when defining

--- src/builtins.c
+++ src/builtins.c
@@ -210,9 +210,10 @@ LispObject *define(LispListElement *arg, LispEnvironment *env)
   ERROR_CHECK;
   debug_message("AFTER NAME TYPE CHECK\n");
 
-  assert_or_error(LispEnvironment_get(env, name->symbol_name, NULL, NULL, NULL), "define", "an object with name %s already exists in environment.", name->symbol_name);
-  ERROR_CHECK;
-  debug_message("AFTER NAME CHECK\n");
+  if (!LispEnvironment_get(env, name->symbol_name, NULL, NULL, NULL)) {
+    LispEnvironment_del_variable(env, name->symbol_name);
+  }
+  Exception_reset();
 
   assert_or_error(arg->next->value->type == LISPOBJECT_LIST, "define", "Argument has wrong type: arglist should be a List, not %s", LispObject_type(arg->next->value));
   ERROR_CHECK;
