07f87f8170cd6561d25cb1b9668665075f296fb0
Chris <chrismjboyle@gmail.com>
2019-11-22 08:59:51 +0000

    Simplified and tidied up tokenise code

--- src/tokenise.c
+++ src/tokenise.c
@@ -5,77 +5,53 @@
 #include "tokenise.h"
 #include "gc.h"
 
-#define ADD_TO_ROOT(R, N, V) \
-  N++; \
-  R = realloc(R, N*sizeof(char*)); \
-  R[N-1] = calloc((strlen(V)+1), sizeof(char)); \
-  strcpy( R[N-1], V ); // why does strncpy complain here?
+#define ADD_TO_TOKENS(VALUE) \
+  (*tokens) = realloc( (*tokens), (++(*n_tokens))*sizeof(char*)); \
+  (*tokens)[(*n_tokens)-1] = calloc((strlen(VALUE)+1), sizeof(char)); \
+  strcpy( (*tokens)[(*n_tokens)-1], VALUE ); // why does strncpy complain here?
 
 
 void tokenise(char *input, char ***tokens, int *n_tokens)
 {
 
-  char **_tokens = NULL;
-  int _n_tokens = 0;
+  char ch;
+  int i = 0;
 
   char *kw_or_name = NULL;
   int kw_or_name_len = 0;
-  int n_open_parens = 0, line=1, col=1;
   int input_len = strlen(input);
 
-  for (int i = 0; i < input_len; i++) {
-    col ++;
+  (*tokens) = NULL;
+  (*n_tokens) = 0;
+
+  for (i = 0, ch= input[0]; i < input_len; i++, ch=input[i]) {
 
     if (input[i] == ';') {
       for (;input[i] != '\n' && i < input_len; i++);
-      col = 0;
-      line++;
+      continue;
     }
     
     // if breaking char: space, newline, or parens
-    if ((input[i] == ' ') || (input[i] == '\n') || (input[i] == ')') || (input[i] == '(')) {
+    if ((ch == ' ') || (ch == '\n') || (ch == ')') || (ch == '(') || (ch == '\t') ) {
 
       // finish reading keyword or name
       if (kw_or_name != NULL) {
         kw_or_name[kw_or_name_len] = '\0';
-        ADD_TO_ROOT(_tokens, _n_tokens, kw_or_name);
+        ADD_TO_TOKENS(kw_or_name);
         kw_or_name = NULL;
         kw_or_name_len = 0;
       }
 
       // action needed on breaking char?
-      switch (input[i]) {
-
-        case '(':
-        // new list
-        ADD_TO_ROOT(_tokens, _n_tokens, "(");
-        n_open_parens ++;
-        break;
-
-      case ')':
-        // close list
-        if (!n_open_parens) {
-          fprintf(stderr, "Unmatched ')'.\n");
-        }
-        ADD_TO_ROOT(_tokens, _n_tokens, ")");
-        n_open_parens --;
-        break;
-
-      case '\n':
-        col = 1;
-        line ++;
-        break;
-
+      if ((ch == '(') || (ch == ')')) {
+        ADD_TO_TOKENS(ch == '(' ? "(" : ")");
       }
 
     } // TODO quote syntactic sugar
     else {
 
-      kw_or_name_len++;
-      kw_or_name = realloc(kw_or_name, (kw_or_name_len + 1)*sizeof(char));
+      kw_or_name = realloc(kw_or_name, ((++kw_or_name_len) + 1)*sizeof(char));
       kw_or_name[kw_or_name_len-1] = input[i];
-
-
 
     }
 
@@ -83,12 +59,9 @@ void tokenise(char *input, char ***tokens, int *n_tokens)
 
   if (kw_or_name != NULL) {
     kw_or_name[kw_or_name_len] = '\0';
-    ADD_TO_ROOT(_tokens, _n_tokens, kw_or_name);
+    ADD_TO_TOKENS(kw_or_name);
     kw_or_name = NULL;
     kw_or_name_len = 0;
   }
-
-  (*tokens) = _tokens;
-  (*n_tokens) = _n_tokens;
 
 }
