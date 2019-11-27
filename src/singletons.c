ac54462873e9826fe09cf5cb99626786619ffbb5
Chris <chrismjboyle@gmail.com>
2019-11-27 16:45:08 +0000

    Added singleton "t" which corresponds to int 1.

--- src/singletons.c
+++ src/singletons.c
@@ -1,3 +1,7 @@
-#include "object.h"
+#include <stdlib.h>
+#include "types.h"
 
+long truth = 1;
+LispAtom t_atom = {&truth, NULL, NULL};
 LispObject nil = {0};
+LispObject t = { &t_atom, NULL, NULL, LISPOBJECT_ATOM, 0, 0, NULL};
