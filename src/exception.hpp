92cf287a9eb86bf50ee133b2f9d49c843328b1f8
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2019-12-02 21:40:16 +0000

    Extend exception

---
+++ src/exception.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <exception>
+#include <string>
+#include <iostream>
+
+#include "colour.hpp"
+
+
+
+
+class Exception : public std::exception {
+  private:
+    std::string detail;
+    std::string type = "";
+  public:
+    Exception(std::string detail, std::string type) { this->detail = detail; this->type = type; }
+
+    void pretty_print() {
+      std::cerr << BOLD << BG_RED << this->type << RESET << ": " << this->detail << std::endl;
+    }
+};
+
+
+
+
+
+#define EXCEPTION(N,S)\
+  class N : virtual public Exception {\
+    public:\
+           N(std::string detail) : Exception(detail, S) { }\
+  };
+
+class ArgumentError : virtual public Exception {
+  public:
+    ArgumentError(std::string detail) : Exception(detail, "ArgumentError") {  }
+};
+
+class NameError : virtual public Exception {
+  public:
+    NameError(std::string detail) : Exception(detail, "NameError") {  }
+};
+
+EXCEPTION(TypeError, "TypeError");
+EXCEPTION(SyntaxError, "SyntaxError");
