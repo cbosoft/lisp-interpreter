7528cf49974b878504772cc8b279cd9251fb3867
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2019-12-03 22:42:20 +0000

    Argument checking in (exit &optional rc)

--- src/builtins/exit.cpp
+++ src/builtins/exit.cpp
@@ -9,14 +9,18 @@ LispObject_ptr exit(LispList_ptr arg, LispEnvironment_ptr env)
   debug_message("builtin function exit");
   (void) env;
 
-  exit(1);
-
   int nargs = arg->count();
   if (nargs > 1) throw ArgumentError(Formatter() << "Wrong number of arguments supplied. In exit: got " << nargs << ", expected no more than 1.");
 
-  LispObject_ptr rc_obj = arg->first();
-  if (rc_obj->get_type() != LISPOBJECT_ATOM) throw ArgumentError(Formatter() << "Argument \"return-code\" should be Atomistic (Int). In exit: got " << nargs << ", expected no more than 1.");
+  int rc = 0;
+  if (nargs == 1) {
+    LispObject_ptr rc_obj = arg->first();
+    if (rc_obj->get_type() != LISPOBJECT_ATOM) throw ArgumentError(Formatter() << "In exit: Argument \"return-code\" should be Atomistic (Int). Got " << rc_obj->repr_type() << ".");
+    LispAtom_ptr rc_atom = rc_obj->get_value_atom();
+    if (rc_atom->get_type() != LISPATOM_INT) throw ArgumentError(Formatter() << "In exit: Argument \"return-code\" should be Atomistic (Int). Got " << rc_atom->repr_type() << ".");
+    rc = rc_atom->get_value_int();
+  }
 
-  return arg->first();
+  exit(rc);
 }
 
