0668ded962396eb6fcb35c7fda6bc5d4dda5cfe8
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2019-12-03 22:35:24 +0000

    Added add func to builtins

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -8,5 +8,15 @@ struct environment_table_row builtin_functions[] = {
 	{ "quote", NULL, NULL, NULL, std::make_shared<LispBuiltin>(LispBuiltin(&quote)), NULL },
 	{ "define", NULL, NULL, NULL, std::make_shared<LispBuiltin>(LispBuiltin(&define)), NULL },
 	{ "exit", NULL, NULL, NULL, std::make_shared<LispBuiltin>(LispBuiltin(&exit)), NULL },
-  { NULL, NULL, NULL, NULL, NULL, NULL } // sentinal
+
+  //maths
+	{ "add", "+", NULL, NULL, std::make_shared<LispBuiltin>(LispBuiltin(&add)), NULL },
+
+
+
+
+
+
+  // sentinel
+  { NULL, NULL, NULL, NULL, NULL, NULL }
 };
