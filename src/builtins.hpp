83b2be8bffc7ef871d033ee1b8ee30cade7acb53
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2019-12-03 22:42:49 +0000

    Added function declarations to header

--- src/builtins.hpp
+++ src/builtins.hpp
@@ -3,3 +3,8 @@
 LispObject_ptr quote(LispList_ptr arg, LispEnvironment_ptr env);
 LispObject_ptr define(LispList_ptr arg, LispEnvironment_ptr env);
 LispObject_ptr exit(LispList_ptr arg, LispEnvironment_ptr env);
+LispObject_ptr add(LispList_ptr arg, LispEnvironment_ptr env);
+LispObject_ptr subtract(LispList_ptr arg, LispEnvironment_ptr env);
+LispObject_ptr multiply(LispList_ptr arg, LispEnvironment_ptr env);
+LispObject_ptr divide(LispList_ptr arg, LispEnvironment_ptr env);
+LispObject_ptr modulo(LispList_ptr arg, LispEnvironment_ptr env);
