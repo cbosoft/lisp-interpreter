0757f138d5b2ed64fae6752615eaa92d9d015798
Chris <chrismjboyle@gmail.com>
2019-12-04 09:07:29 +0000

    Corrected error string in add function

--- src/builtins/add.cpp
+++ src/builtins/add.cpp
@@ -15,9 +15,9 @@ LispObject_ptr add(LispList_ptr arg, LispEnvironment_ptr env)
   if (nargs != 2) throw ArgumentError(Formatter() << "In " << FUNC << ": Wrong number of arguments supplied. Got " << nargs << ", expected 2.");
 
   LispObject_ptr left = arg->next(true);
-  if (left->get_type() != LISPOBJECT_ATOM) throw ArgumentError(Formatter() << "In " << FUNC << ": Argument \"left\" should be Atomistic (Int). Got " << left->repr_type() << ".");
+  if (left->get_type() != LISPOBJECT_ATOM) throw ArgumentError(Formatter() << "In " << FUNC << ": Argument \"left\" should be Atomistic. Got " << left->repr_type() << ".");
   LispObject_ptr right = arg->next();
-  if (right->get_type() != LISPOBJECT_ATOM) throw ArgumentError(Formatter() << "In " << FUNC << ": Argument \"right\" should be Atomistic (Int). Got " << right->repr_type() << ".");
+  if (right->get_type() != LISPOBJECT_ATOM) throw ArgumentError(Formatter() << "In " << FUNC << ": Argument \"right\" should be Atomistic. Got " << right->repr_type() << ".");
 
   LispAtom_ptr left_atom = left->get_value_atom();
   LispAtom_ptr right_atom = right->get_value_atom();
