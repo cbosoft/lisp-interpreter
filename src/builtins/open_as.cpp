cd65b5a16bb1348902ca6284fb90f03922a94eef
Chris <chrismjboyle@gmail.com>
2019-12-09 08:54:14 +0000

    Open file using fcntl's open (man 3 open) to get an int file descriptor which can be entered into the environment as a variable

--- src/builtins/open_as.cpp
+++ src/builtins/open_as.cpp
@@ -1,3 +1,7 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include "../types.hpp"
 #include "../formatter.hpp"
 #include "../debug.hpp"
@@ -21,30 +25,35 @@ LispObject_ptr while_open(LispList_ptr arg, LispEnvironment_ptr env)
   LispAtom_ptr path_atom = path_obj->get_value_atom();
   if (path_atom->get_type() != LISPATOM_STRING)
     throw TypeError(Formatter() << "In " << FUNC << ": Argument \"path\" should be Atomistic (String). Got " << path_atom->repr_type() << ".");
+  const char* path_cstr = path_atom->get_value_string().c_str();
 
   LispObject_ptr mode_obj = arg->next(true);
   if (mode_obj->get_type() != LISPOBJECT_ATOM)
-    throw TypeError(Formatter() << "In " << FUNC << ": Argument \"mode\" should be Atomistic (String). Got " << mode_obj->repr_type() << ".");
+    throw TypeError(Formatter() << "In " << FUNC << ": Argument \"mode\" should be Atomistic (String): \"r\", \"w\", or \"a\". Got " << mode_obj->repr_type() << ".");
 
   LispAtom_ptr mode_atom = mode_obj->get_value_atom();
   if (mode_atom->get_type() != LISPATOM_STRING)
-    throw TypeError(Formatter() << "In " << FUNC << ": Argument \"mode\" should be Atomistic (String). Got " << mode_atom->repr_type() << ".");
+    throw TypeError(Formatter() << "In " << FUNC << ": Argument \"mode\" should be Atomistic (String): \"r\", \"w\", or \"a\". Got " << mode_atom->repr_type() << ".");
 
   std::string mode_str = mode_atom->get_value_string();
-  // TODO for each char in str, append mode
-  std::ios_base::openmode mode;
+  if (mode_str.size() != 1)
+    throw ArgumentError(Formatter() << "In " << FUNC << ": Argument \"mode\" should be Atomistic (String): \"r\", \"w\", or \"a\". Got " << mode_atom->repr() << ".");
+  char mode_ch = mode_str[0];
+  int oflags = 0;
+
   switch (mode_ch) {
-    case 'w':
-      mode = std::ios::app;
-      break;
     case 'r':
-      mode = std::ios::;
+      oflags = O_RDONLY;
       break;
+
     case 'a':
-      mode = std::ios::app;
+      oflags = O_APPEND;
+      // fall through
+    case 'w':
+      oflags |= O_WRONLY;
       break;
   }
-  
+
 
   LispObject_ptr name_obj = arg->next();
   if (path_obj->get_type() != LISPOBJECT_SYMBOL)
@@ -54,8 +63,14 @@ LispObject_ptr while_open(LispList_ptr arg, LispEnvironment_ptr env)
 
   LispList_ptr body = arg->rest()->rest();
 
-  LispFileIO fp(name_symb->get_name(), std::);
+  int fd = open(path_cstr, oflags);
+  if (fd < 0)
+    throw IOError(Formatter() << "Could not open file \"" << path_atom->get_value_string() << "\" (" << errno << ") " << strerror(errno));
 
+  LispEnvironment_ptr subenv = std::make_shared<LispEnvironment>(LispEnvironment(env));
+  subenv->add(name_symb->get_name(), std::make_shared<LispObject>(LispObject((long)fd)));
 
-  return 
+  LispObject_ptr rv = body->eval_each(subenv);
+  close(fd);
+  return rv;
 }
