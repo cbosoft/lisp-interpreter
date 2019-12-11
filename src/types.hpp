f4e6c3468528f8e224df4220dcd17fbb2e52387e
Chris <chrismjboyle@gmail.com>
2019-12-11 16:00:41 +0000

    is_macro is const method

--- src/types.hpp
+++ src/types.hpp
@@ -330,7 +330,7 @@ class Executable {
     Executable() { this->_is_macro = false; }
     ~Executable() { }
     void set_macro() { this->_is_macro = true; }
-    bool is_macro() { return this->_is_macro; }
+    bool is_macro() const { return this->_is_macro; }
     virtual LispObject_ptr eval(LispList_ptr arg, LispEnvironment_ptr env) =0;
 };
 
