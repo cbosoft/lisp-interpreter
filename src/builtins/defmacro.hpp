0c0af0cb60c33672d4b92707327521416ff6dd67
Chris <chrismjboyle@gmail.com>
2019-12-20 12:11:06 +0000

    refactor: use new exception type- and arg-checking helper functions

--- src/builtins/defmacro.hpp
+++ src/builtins/defmacro.hpp
@@ -1,13 +1,15 @@
 #include "../debug.hpp"
 #include "../types.hpp"
+#include "../exception_check.hpp"
 #include "../formatter.hpp"
 #include "../pointer.hpp"
 
 
 class LispFunc_defmacro : LispBuiltin {
   private:
-    const std::string doc = 
-      "(defmacro name args &rest body)\n\
+    const std::string name = "defmacro";
+    const std::string doc = \
+      "(defmacro name list &rest body)\n\
       Creates a new macro with NAME in environment. ARGS is a list of symbols which \n\
       are arguments to the macro. These are NOT evaluated before the macro is run.\n\
       This is the difference between a macro and a function.";
@@ -18,41 +20,35 @@ class LispFunc_defmacro : LispBuiltin {
       this->set_macro();
     }
 
-    std::string repr()
+    std::string repr() const
     {
       return "Func(defmacro,builtin)";
     }
 
-    LispObject_ptr eval(LispList_ptr arg, LispEnvironment_ptr env)
+    LispObject_ptr eval(LispList_ptr arg, LispEnvironment_ptr env) const
     {
-      debug_message("builtin function defmacro");
-    
-      int nargs = arg->count();
-      if (nargs < 3) throw ArgumentError(Formatter() << "Wrong number of arguments supplied. In defmacro: got " << nargs << ", expected at least 3.");
-     
-      auto arg_iter = arg->begin();
-      LispObject_ptr name = (*arg_iter);
-      if (name->get_type() != LISPOBJECT_SYMBOL) throw TypeError(Formatter() << "Argument has wrong type: name should be a Symbol, not " << name->repr_type() << ".");
-    
-      arg_iter++;
-      LispObject_ptr arglist = (*arg_iter);
-      if (arglist->get_type() != LISPOBJECT_LIST) throw TypeError(Formatter() << "Argument has wrong type: arglist should be a List, not " << arglist->repr_type());
-    
-      LispList_ptr argnames_list = arglist->get_value_list();
+      narg_check_min(arg, 3, this->name, "name args &rest body");
+
+      LispObject_ptr name_obj = arg->next(true);
+      type_check_one(name_obj, LISPOBJECT_SYMBOL, this->name, "name");
+
+      LispObject_ptr arglist_obj = arg->next();
+      type_check_one(arglist_obj, LISPOBJECT_LIST, this->name, "arglist");
+
+      LispList_ptr argnames_list = arglist_obj->get_value_list();
       auto argnames_iter = argnames_list->begin();
       for (; argnames_iter != argnames_list->end(); ++argnames_iter) {
-        if ((*argnames_iter)->get_type() != LISPOBJECT_SYMBOL) throw TypeError(Formatter() << "Argument has wrong type: arglist items should be Symbols, not " << (*argnames_iter)->repr_type());
+        type_check_one(*argnames_iter, LISPOBJECT_SYMBOL, this->name, "arg");
       }
-      debug_message("after arglist_items check");
     
-      std::string name_str = name->get_value_symbol()->get_name();
-      LispList_ptr body = arg->rest()->rest();
+      std::string name_str = name_obj->get_value_symbol()->get_name();
+      LispList_ptr body = arg->rest(2);
       LispFunction_ptr lfunc = std::make_shared<LispFunction>(LispFunction(argnames_list, body, name_str, "TODO"));
       env->add_something(name_str, NULL, NULL, lfunc);
-      return name;
+      return name_obj;
     }
 
-    std::string get_doc() {
+    const std::string get_doc() const {
       return this->doc;
     }
 };
